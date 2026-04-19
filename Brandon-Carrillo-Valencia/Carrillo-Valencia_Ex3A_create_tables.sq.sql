fadae22b845335601c69e06b740f59667c170c54
Bcv1018 <carrillobrandon98@gmail.com>
2026-04-19 04:04:09 -0700

    Added Folders and moved files into them

---
+++ Brandon-Carrillo-Valencia/Carrillo-Valencia_Ex3A_create_tables.sq.sql
@@ -0,0 +1,37 @@
+DROP SCHEMA IF EXISTS lana_dog_walking;
+CREATE SCHEMA lana_dog_walking;
+USE lana_dog_walking;
+
+-- CustomerInfo Table
+CREATE TABLE CustomerInfo (
+    CustomerID INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Address VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL,
+    PaymentMethod VARCHAR(50) NOT NULL,
+    PRIMARY KEY (CustomerID)
+);
+
+-- Pet Info Table
+CREATE TABLE PetInfo (
+    PetID INT NOT NULL AUTO_INCREMENT,
+    CustomerID INT NOT NULL,
+    PetName VARCHAR(100) NOT NULL,
+    PetBreed VARCHAR(100) NOT NULL,
+    PetColor VARCHAR(50) NOT NULL,
+    PRIMARY KEY (PetID),
+    CONSTRAINT FK_Customer FOREIGN KEY (CustomerID) REFERENCES CustomerInfo(CustomerID)
+);
+
+-- Services/charges Table
+CREATE TABLE ServicesCharges (
+    SessionID INT NOT NULL AUTO_INCREMENT,
+    CustomerID INT NOT NULL,
+    PetID INT NOT NULL,
+    SessionDate DATETIME NOT NULL,
+    SessionRate DECIMAL(10,2) NOT NULL,
+    SessionQty INT NOT NULL,
+    PRIMARY KEY (SessionID),
+    CONSTRAINT FK_Services_Customer FOREIGN KEY (CustomerID) REFERENCES CustomerInfo(CustomerID),
+    CONSTRAINT FK_Services_Pet FOREIGN KEY (PetID) REFERENCES PetInfo(PetID)
+);
